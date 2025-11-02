95b0fa6b73eab5e7741a7a909373f32264e5fa9a
Marc Kupietz <kupietz@ids-mannheim.de>
2025-11-02 19:54:11 +0100

    Bump version to v2.20
    Change-Id: I581ace3cadc1c198d41e969eea2d89017b652e01

--- app/src/main/kotlin/de/ids_mannheim/korapxmltools/KorapXmlTool.kt
+++ app/src/main/kotlin/de/ids_mannheim/korapxmltools/KorapXmlTool.kt
@@ -51,7 +51,7 @@ val ZIP_ENTRY_UNIX_MODE = parseInt("644", 8)
 @Command(
     name = "KorapXmlTool",
     mixinStandardHelpOptions = true,
-    version = ["KorapXmlTool 2.01"],
+    version = ["KorapXmlTool 2.20"],
     description = ["Converts KorAP-XML <https://github.com/KorAP/KorAP-XML-Krill#about-korap-xml> base or " +
             "morpho zips to (annotated) CoNLL(-U) format with all information necessary for " +
             "reconstruction in comment lines."]
